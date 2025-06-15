7011c678a055055b428ab5c25ab616122d1f07db
Rochu96 <mohitrochlani09@gmail.com>
2025-06-15 12:08:02 +0530

    Revert "added a new function sum"
    This reverts commit d376b547b465b9b68ecc72baa5fa4c29a1a29f01.

--- data_processing.py
+++ data_processing.py
@@ -1,11 +1,2 @@
 #fraud_detection
 print("here all the data processing code will be written")
-
-a=4
-b=3
-
-def sum(a,b):
-    c = a+b
-    return c
-
-print("added a new function sum")
--- trying_something.py
+++
@@ -1,1 +0,0 @@
-print("traing the commit without -m")
