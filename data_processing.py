d376b547b465b9b68ecc72baa5fa4c29a1a29f01
Rochu96 <mohitrochlani09@gmail.com>
2025-06-15 12:04:51 +0530

    added a new function sum

--- data_processing.py
+++ data_processing.py
@@ -1,2 +1,11 @@
 #fraud_detection
 print("here all the data processing code will be written")
+
+a=4
+b=3
+
+def sum(a,b):
+    c = a+b
+    return c
+
+print("added a new function sum")
---
+++ trying_something.py
@@ -0,0 +1,1 @@
+print("traing the commit without -m")
